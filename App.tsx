b7c1929bf688268563b816e65242b13cfffdde1a
nailson <nailson.ivs@codens.com.br>
2023-02-09 17:10:40 -0300

    add styled component

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import { StatusBar, View } from 'react-native';
 
 import * as ScreenOrientation from 'expo-screen-orientation';
 import { Home } from './src/screens/Home/index';
+import {useFonts, Poppins_400Regular, Poppins_600SemiBold, Poppins_700Bold} from '@expo-google-fonts/poppins';
 
 
 ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT)
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Poppins_400Regular, Poppins_600SemiBold, Poppins_700Bold})
+
   return (
     <>
       <StatusBar 
